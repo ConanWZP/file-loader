3e38b260ff083408b4353a340f8a0041a149c7d1
ConanWZP <yaroslav2281337@mail.ru>
2023-08-17 09:48:52 +0300

    Finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {PickerOverlay} from 'filestack-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+
+    const [isShowed, setIsShowed] = useState(false)
+
+    const [uploadedFiles, setUploadedFiles] = useState<any[]>([])
+
+
+    const handleUpload = (res: any) => {
+        console.log(res)
+        const updLoadedFiles = [...uploadedFiles]
+        res.filesUploaded.map(({handle, filename}: any) => {
+            const newFile = {
+                handle,
+                filename,
+                timeStamp: new Date().toLocaleString()
+            }
+            updLoadedFiles.push(newFile)
+
+        })
+        localStorage.setItem('updStorage', JSON.stringify(updLoadedFiles))
+        setUploadedFiles(updLoadedFiles)
+        setIsShowed(false)
+    }
+
+    const deleteFile = (fileId: string) => {
+        const updLoadedFiles = uploadedFiles.filter((file) => file.handle !== fileId)
+        localStorage.setItem('updStorage', JSON.stringify(updLoadedFiles))
+        setUploadedFiles(updLoadedFiles)
+    }
+
+    useEffect(() => {
+
+        const filesLocalStorage = JSON.parse(localStorage.getItem('updStorage') || '') || []
+        setUploadedFiles(filesLocalStorage)
+
+
+    }, [])
+
+    return (
+        <div className='main'>
+            <h1 className={`title`}>F - Loader</h1>
+            <p>Upload your files</p>
+            <button className={`btn`} onClick={() => setIsShowed(!isShowed)}>Upload</button>
+            <div className={`uploaded-files`}>
+                <h3>Uploaded files:</h3>
+                {
+                    uploadedFiles.length === 0 ?
+                        <p>No files have been uploaded</p>
+                        :
+                        <ul>
+                            {uploadedFiles.map(({handle, filename, timeStamp}) => (
+                                <li key={handle}>
+                                    <span>{filename}</span>
+                                    <span>{timeStamp}</span>
+                                    <button className={`redirect-button`} onClick={() => window.open(`https://cdn.filestackcontent.com/${handle}`)}>Open</button>
+                                    <button className={`delete-button`} onClick={() => deleteFile(handle)}>Delete</button>
+                                </li>
+                            ))}
+                        </ul>
+                }
+            </div>
+            {
+                isShowed ?
+                    <div>
+
+
+                        <PickerOverlay
+                            apikey={'AYmFAc2thRwqTF7AK7xe4z'}
+
+                            onUploadDone={(res) => handleUpload(res)}
+                        />
+                    </div>
+                    :
+                    null
+            }
+        </div>
+    )
+
+};
 
 export default App;
